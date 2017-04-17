f14a12465e69180d99e5e4ab478721e8ddce29d0
tianr <ruitian2004@hotmail.com>
2017-04-17 17:59:19 +0800

    motifVar

---
+++ 10motifVar/motifVar.sh
@@ -0,0 +1,16 @@
+#2017 April 17
+
+#motif database, position weighted matrix
+#bedtools getfasta
+#motif scanning
+#match scoring, log ratio???
+#speed!
+
+wget http://homer.ucsd.edu/homer/custom.motifs
+
+#the other rows are the positions specific probabilities for 
+#each nucleotide (A/C/G/T). 
+
+#get fasta file from VCF/BED
+bedtools getfasta -fi /home/tianr/02databases/bwa_indexes/hg19/hg19.fa -bed ~/Desktop/test.bed -fo ~/Desktop/test.fa
+ 
